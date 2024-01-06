df1135c5661bffde6d2e8ef0da3b69a362d612df
lh <lh@server7>
2024-01-06 14:54:53 +0000

    fix: WGAN

--- models/WGAN.py
+++ models/WGAN.py
@@ -52,6 +52,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(),
 
             nn.Conv2d(hidden_dim*4,1,4,1,0),
+
         )
 
     def execute(self, x):
@@ -71,7 +72,7 @@ def circle(iterable):
             yield x
 
 class WGAN_Manager():
-    def __init__(self, train_loader, test_loader, num_steps, wandb_run, critic_steps=5) -> None:
+    def __init__(self, train_loader, test_loader, wandb_run, num_steps=10000, critic_steps=5) -> None:
         self.netG=Generator()
         self.netD=Discriminator()
         self.train_loader=train_loader
@@ -93,34 +94,46 @@ class WGAN_Manager():
 
         for i in tqdm(range(self.num_steps), desc='Training'):
 
+            loss_D=[]
             # 训练辨别器
             for _ in range(0, self.critic_steps):
-                real=next(train_gen)
-                noise=np.random.randn(real.shape[0],3,1,1)
+                real , label=next(train_gen)
+                # 注意这里必须全都是32类型
+                real=jt.transpose(real, (0, 3, 1, 2)).float32()
+                noise=jt.float32(np.random.randn(real.shape[0],16,1,1))
 
                 critic_real_mean=self.netD(real).mean()
                 critic_gen_mean=self.netD(self.netG(noise)).mean()
 
                 loss=critic_gen_mean-critic_real_mean
+                loss.sync()
                 self.optimizerD.step(loss)
+                loss_D.append(loss.data)
 
-                self.netD.safe_clip(-0.01,0.01)
+                for param in  self.netD.parameters():
+                    param.safe_clip(-0.01,0.01)
 
-                if i%100==0:
-                    self.wandb_run.log({'lossD':loss.data[0]}, step=i)
-                    print(loss.data[0])
+            
             
             # 训练生成器
-            noise=np.random.randn(64,3,1,1)
+            noise=jt.float32(np.random.randn(64,16,1,1))
             gen=self.netG(noise)
             loss=-self.netD(gen).mean()
+            loss.sync()
             self.optimizerG.step(loss)
 
-            self.netD.safe_clip(-0.01,0.01)
+            for param in  self.netG.parameters():
+                param.safe_clip(-0.01,0.01)
 
-            if i%100==0:
+            if i%100==0: 
+                print(f"    step {i}:")
+                print(f"        lossD: {np.mean(loss_D)}")
+                print(f"        lossG: {loss.data[0]}")
+
+                self.wandb_run.log({'lossD':np.mean(loss_D)}, step=i)
                 self.wandb_run.log({'lossG':loss.data[0]}, step=i)
-                self.wandb_run.log({'img':[wandb.Image(gen[j].transpose(1,2,0).numpy()) for j in range(0,5)]}, step=i)
+                self.wandb_run.log({'img':[wandb.Image(gen[j].transpose(1,2,0).numpy()) for j in np.random.choice(64,5, replace=False)]}, step=i)
+
 
     def test(self):
         
@@ -128,7 +141,7 @@ class WGAN_Manager():
         self.netD.eval()
 
 
-        noise=np.random.randn(16,3,1,1)
+        noise=jt.float32(np.random.randn(16,16,1,1))
         imgs=self.netG(noise).data
         for index, img in enumerate(imgs):
             img=img.transpose(1,2,0)
@@ -138,14 +151,18 @@ class WGAN_Manager():
 
 
 if __name__ == "__main__":
+    if jt.has_cuda:
+        jt.flags.use_cuda=1
+
+
     wandb_run=wandb.init(
         project='image-gen',
     )
 
 
-    train_loader=CIFAR10(train=True, shuffle=True, batch_size=64)
-    test_loader=CIFAR10(train=False, shuffle=False, batch_size=64)  
+    train_loader=CIFAR10(train=True).set_attrs(shuffle=True, batch_size=64)
+    test_loader=CIFAR10(train=False).set_attrs(shuffle=True, batch_size=64)
 
 
-    manager=WGAN_Manager(train_loader, test_loader, 10000, wandb_run=wandb_run)
+    manager=WGAN_Manager(train_loader, test_loader,wandb_run=wandb_run)
     manager.train()
